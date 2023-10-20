96568020a25dfcebe2eec6e08b323bf8abe07410
unknown <463997479@qq.com>
2023-10-20 14:09:08 +0800

    fix: 修改代码规范

--- packages/components/src/button/index.js
+++ packages/components/src/button/index.js
@@ -2,11 +2,10 @@
  * @Date: 2023-10-18 09:36:50
  * @Auth: 463997479@qq.com
  * @LastEditors: 463997479@qq.com
- * @LastEditTime: 2023-10-18 11:36:09
- * @FilePath: \dcqc-component\packages\components\src\button\index.js
+ * @LastEditTime: 2023-10-20 14:08:50
+ * @FilePath: \dc-component\packages\components\src\button\index.js
  */
-import button from "./button.vue";
-import { withInstall } from "dcqc-utils";
-//app.use(注册组件)
-const DButton = withInstall(button);
-export default DButton;
+import button from './button.vue'
+import { withInstall } from 'dcqc-utils'
+const DButton = withInstall(button)
+export default DButton
