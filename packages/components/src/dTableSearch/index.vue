3e068f3a2b1a6546d5a02ce42d1cc20dd6015732
unknown <463997479@qq.com>
2023-10-30 18:17:14 +0800

    feat: 新增表格搜索

--- packages/components/src/dTableSearch/index.vue
+++ packages/components/src/dTableSearch/index.vue
@@ -2,7 +2,7 @@
  * @Date: 2023-10-30 10:58:31
  * @Auth: 463997479@qq.com
  * @LastEditors: 463997479@qq.com
- * @LastEditTime: 2023-10-30 17:43:37
+ * @LastEditTime: 2023-10-30 18:16:16
  * @FilePath: \dc-component\packages\components\src\dTableSearch\index.vue
 -->
 <template>
@@ -10,10 +10,10 @@
     <el-card class="box-card">
       <template v-if="hasSearch" #header>
         <div class="card-header">
-          <el-form :model="searchForm">
+          <el-form ref="ruleFormRef" :model="searchForm">
             <slot :searchForm="searchForm" name="searchData"></slot>
             <el-button type="primary" @click="handleSearch">查询</el-button>
-            <el-button @click="handleReset">重置</el-button>
+            <el-button @click="handleReset(ruleFormRef)">重置</el-button>
           </el-form>
         </div>
       </template>
@@ -35,7 +35,7 @@
           ></el-table-column>
         </el-table>
         <div class="d-table-footer">
-          <el-pagination v-bind="{ ...props.pagination }" />
+          <el-pagination v-bind="{ ...pagination }" />
         </div>
       </div>
     </el-card>
@@ -44,6 +44,8 @@
 
 <script lang="ts" name="DTableSearch" setup>
 import type { ColumProps } from '@/components';
+import type { FormInstance } from 'element-plus';
+
 import {
   ElPagination,
   ElButton,
@@ -65,17 +67,19 @@ let columArr: any[] = [];
 const tableData = ref([]);
 //查询条件
 const searchForm = ref({});
-
-const props = withDefaults(
+const ruleFormRef = ref<FormInstance>();
+const { request, hasPage, columns, pagination } = withDefaults(
   defineProps<{
     columns?: ColumProps[];
     request: (params: any, done: (data: any[]) => void) => void;
     title?: string;
     pagination?: any; //分页所有参数
     hasSearch?: boolean; //是否需要搜索
+    hasPage?: boolean;
   }>(),
   {
     hasSearch: false,
+    hasPage: true,
   },
 );
 //搜索
@@ -83,23 +87,31 @@ const handleSearch = () => {
   handleMethod();
 };
 //重置
-const handleReset = () => {};
+const handleReset = (formEl: FormInstance | undefined) => {
+  if (hasPage) {
+    searchForm.value = {
+      ...searchForm.value,
+      pageNum: 10,
+      pageSize: 1,
+    };
+  }
+  if (!formEl) return;
+  formEl.resetFields();
+  console.log(searchForm.value);
+  handleMethod();
+};
 //异步函数返回
 
 const handleMethod = (): void => {
-  props.request({ ...searchForm.value }, data => {
+  request({ ...searchForm.value }, data => {
     tableData.value = data;
-
-    for (let key of props.columns) {
-      console.log(key);
-    }
   });
 };
 
 //循环处理colum中数据格式
 const handleResetColum = (): void => {
   columArr = [];
-  for (let key of props.columns) {
+  for (let key of columns) {
     let obj = {};
 
     for (let k in key as any) {
