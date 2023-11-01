52af0aa537b05dd461409da6bacfd9dd67e5e4b5
bchen.al <148536075+bchen-al@users.noreply.github.com>
2023-11-01 15:19:33 +0800

    feat: .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -6,20 +6,31 @@
  * @FilePath: \dc-component\.prettierrc.js
  */
 module.exports = {
-  singleQuote: true,
   printWidth: 80,
-  pluginSearchDirs: false,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
-  proseWrap: 'never',
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
   singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
   trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  vueIndentScriptAndStyle: false,
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'auto',
+  singleAttributePerLine: false,
   overrides: [
     {
       files: '*.md',
       options: {
-        proseWrap: 'preserve'
-      }
-    }
-  ]
-}
+        proseWrap: 'preserve',
+      },
+    },
+  ],
+};
