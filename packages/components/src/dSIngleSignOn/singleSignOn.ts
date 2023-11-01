3ae33d08c46e7cd2b8b776e0c72e11f042b02761
bchen.al <148536075+bchen-al@users.noreply.github.com>
2023-11-01 17:38:31 +0800

    feat: 单点登录更新props定义方式

--- packages/components/src/dSIngleSignOn/singleSignOn.ts
+++ packages/components/src/dSIngleSignOn/singleSignOn.ts
@@ -1,25 +1,26 @@
-export const singleSignOnProps = {
+import { type AxiosInstance } from 'axios';
+
+export interface singleSignOnPropsInterface {
   /** @description 单点登录api地址 */
-  api: {
-    type: String,
-    required: false,
-    default: '/single-sign-on',
-  },
-  /** @description 单点登录成功后跳转地址 */
-  target: {
-    type: String,
-    default: '/',
-  },
-  /** @description 标识符名称 */
-  token: {
-    type: String,
-    default: 'token',
-  },
+  api: string;
   /** @description 字符串参数名称 */
-  suffix: {
-    type: String,
-    default: 'auth_code',
-  },
+  query?: string;
+  /** @description 单点登录请求标识符名称 */
+  token?: string;
+  /** @description 单点登录成功后跳转地址 */
+  target?: string;
+  /** @description Axios实例 */
+  axiosInstance?: AxiosInstance;
+}
+export const singleSignOnPropsDefaults = {
+  /** @description 单点登录api地址 */
+  api: '/single-sign-on',
+  /** @description 字符串参数名称 */
+  query: 'auth_code',
+  /** @description 单点登录请求标识符名称 */
+  token: 'token',
+  /** @description 单点登录成功后跳转地址 */
+  target: '/',
 };
 
 export const singleSignOnEmits = ['response'];
