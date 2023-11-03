db34a5cb7091799b94c9f45ef60cb21ddeadd576
unknown <463997479@qq.com>
2023-11-03 16:10:05 +0800

    feat: dTableSearch 1.0.0

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,58 +2,55 @@
  * @Date: 2023-10-31 16:46:45
  * @Auth: 463997479@qq.com
  * @LastEditors: 463997479@qq.com
- * @LastEditTime: 2023-10-31 17:43:24
+ * @LastEditTime: 2023-11-03 16:09:20
  * @FilePath: \dc-component\src\router\index.ts
  */
-import {
-  createRouter,
-  createWebHistory,
-} from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 // 2. 定义一些路由：每个路由都需要映射到一个组件。
 import DTableSearch from '../view/dTableSearch/index.vue';
 import LayoutComponent from '../layout/index.vue';
 import Bak from '../view/bak/index.vue';
 import SingleSignOn from '../view/SingleSignOn/index.vue';
-import Modal from '../view/Modal/index.vue'
-import dRelationGraph from '../view/dRelationGraph/index.vue'
+import Modal from '../view/Modal/index.vue';
+import dRelationGraph from '../view/dRelationGraph/index.vue';
 
 export const routes: any = [
   {
     path: '/',
     component: LayoutComponent,
-    redirect: '/dashboard',
+    redirect: '/dTableSearch',
 
     children: [
       {
-        path: 'dashboard',
-        name: 'Dashboard',
+        path: 'dTableSearch',
+        name: 'DTableSearch',
 
-        meta: { title: '首页', affix: true },
+        meta: { title: 'DTableSearch' },
         component: DTableSearch,
       },
       {
         path: 'bak',
         name: 'bak',
-        meta: { title: '首页', affix: true },
+        meta: { title: 'bak' },
         component: Bak,
       },
       {
         path: 'SingleSignOn',
         name: 'SingleSignOn',
-        meta: { title: '首页', affix: true },
+        meta: { title: '首页' },
         component: SingleSignOn,
       },
       {
         path: 'Modal',
         name: 'Modal',
-        meta: { title: '首页', affix: true },
+        meta: { title: '首页' },
         component: Modal,
       },
       {
         path: 'dRelationGraph',
         name: 'dRelationGraph',
-        meta: { title: '首页', affix: true },
+        meta: { title: '首页' },
         component: dRelationGraph,
       },
     ],
