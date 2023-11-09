7dacdaefc0294e8feaeae1ba464fd5a0e8188fcf
bchen.al <148536075+bchen-al@users.noreply.github.com>
2023-11-09 10:33:42 +0800

    fix(单点登录): 缺少请求标识符时返回异步请求

--- packages/components/src/dSIngleSignOn/src/singleSignOn.vue
+++ packages/components/src/dSIngleSignOn/src/singleSignOn.vue
@@ -63,7 +63,8 @@ function handleSingleSignOn() {
   } else {
     status.value = 'failed';
     message.value = '缺少请求标识符';
-    return Promise.reject('token-not-found');
+    emit('response-promise', Promise.reject('query-token-not-found'));
+    return Promise.reject('query-token-not-found');
   }
 }
 
